1445496f1fda270d1815f3295bfcb50bdd12cdd4
Ted Kaemming <ted@kaemming.com>
2020-05-29 12:02:34 -0700

    tidy up scheduler metrics

--- snuba/subscriptions/scheduler.py
+++ snuba/subscriptions/scheduler.py
@@ -7,6 +7,7 @@ from typing import Generic, Iterator, List, Optional, TypeVar
 from snuba.subscriptions.data import PartitionId, Subscription, SubscriptionIdentifier
 from snuba.subscriptions.store import SubscriptionDataStore
 from snuba.utils.metrics.backends.abstract import MetricsBackend
+from snuba.utils.metrics.backends.wrapper import MetricsWrapper
 from snuba.utils.types import Interval
 
 
@@ -56,7 +57,9 @@ class SubscriptionScheduler(Scheduler[Subscription]):
         self.__store = store
         self.__cache_ttl = cache_ttl
         self.__partition_id = partition_id
-        self.__metrics = metrics
+        self.__metrics = MetricsWrapper(
+            metrics, "schedule", tags={"partition": str(self.__partition_id)}
+        )
 
         self.__subscriptions: List[Subscription] = []
         self.__last_refresh: Optional[datetime] = None
@@ -73,16 +76,10 @@ class SubscriptionScheduler(Scheduler[Subscription]):
                 for uuid, data in self.__store.all()
             ]
             self.__last_refresh = current_time
-            self.__metrics.gauge(
-                "schedule.size",
-                len(self.__subscriptions),
-                tags={"partition": str(self.__partition_id)},
-            )
+            self.__metrics.gauge("size", len(self.__subscriptions))
 
         self.__metrics.timing(
-            "schedule.staleness",
-            (current_time - self.__last_refresh).total_seconds() * 1000.0,
-            tags={"partition": str(self.__partition_id)},
+            "staleness", (current_time - self.__last_refresh).total_seconds() * 1000.0
         )
 
         return self.__subscriptions
